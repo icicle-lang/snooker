dedbcb73e817ffd76e0d05d72d2b0507e8a557da
Jacob Stanley <jacob@stanley.io>
2016-04-15 10:06:07 +1000

    Conduit encode and tests for conduit encode/decode

--- test/Test/Snooker/Util.hs
+++ test/Test/Snooker/Util.hs
@@ -1,18 +1,22 @@
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE NoImplicitPrelude #-}
 module Test.Snooker.Util (
     binaryTripping
+  , squashRender
   ) where
 
 import           Data.Binary.Get (Get)
 import qualified Data.Binary.Get as Binary
 import           Data.ByteString.Builder (Builder)
 import qualified Data.ByteString.Builder as Builder
+import qualified Data.Text as T
 
 import           Disorder.Core.Tripping (tripping)
 
 import           P
 
 import           Test.QuickCheck (Property)
+import           Test.QuickCheck.Property (counterexample, failed)
 
 
 binaryTripping :: (Show a, Eq a) => (a -> Builder) -> Get a -> a -> Property
@@ -23,3 +27,12 @@ binaryTripping encode decode =
     tripping
       (Builder.toLazyByteString . encode)
       (bimap third third . Binary.runGetOrFail decode)
+
+squashRender :: Show x => (x -> Text) -> Either x Property -> Property
+squashRender render = \case
+  Left err ->
+    counterexample (show err) $
+    counterexample (T.unpack $ render err) $
+    failed
+  Right prop ->
+    prop
